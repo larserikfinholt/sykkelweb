771d3ac635703e24615d53974b415126c74e2c81
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2014-02-12 23:00:06 +0100

    trip service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
 angular.module('sykkelwebApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+  .controller('MainCtrl', function ($scope, TripService, $http) {
+      $scope.start = function () {
+          TripService.startNew('jalla', 'balla', 'ddd').success(function (d) {
+              console.log("Started", d);
+          });
+      }
+      $http.get('/api/awesomeThings').success(function (awesomeThings) {
+          $scope.awesomeThings = awesomeThings;
+      });
+      TripService.getLatest().success(function (trips) {
+          //$http.get('/api/trips').success(function (trips) {
+          $scope.trips = trips;
+      });
+
   });
---
+++ test/spec/server/triptest.js
@@ -0,0 +1,9 @@
+'use strict';
+var trips = require('/lib/controllers/trips');
+
+describe('Server: trip controller', function () {
+
+  it('should be able to create a new trip', function () {
+    console.log('test.............', trips);
+  });
+});
