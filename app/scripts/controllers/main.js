8971d745829289e507bb72279c4a80e0e0d1624a
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2014-02-16 19:58:10 +0100

    jshint fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('sykkelwebApp')
-  .controller('MainCtrl', function ($scope, TripService, $http) {
-     
-      $http.get('/api/awesomeThings').success(function (awesomeThings) {
-          $scope.awesomeThings = awesomeThings;
-      });
-      TripService.getLatest().success(function (trips) {
-          //$http.get('/api/trips').success(function (trips) {
-          $scope.trips = trips;
-      });
+    .controller('MainCtrl', function ($scope, TripService, $http) {
 
-  });
+        $http.get('/api/awesomeThings').success(function (awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+        });
+        TripService.getLatest().success(function (trips) {
+            //$http.get('/api/trips').success(function (trips) {
+            $scope.trips = trips;
+        });
+
+    });
--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('sykkelwebApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }
-    //, {
-    //  'title': 'Settings',
-    //  'link': '/settings'
-    //}
-    ];
-    
-    $scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+    .controller('NavbarCtrl', function ($scope, $location, Auth) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
+        }
+        //, {
+        //  'title': 'Settings',
+        //  'link': '/settings'
+        //}
+        ];
+
+        $scope.logout = function () {
+            Auth.logout()
+            .then(function () {
+                $location.path('/login');
+            });
+        };
+
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+    });
